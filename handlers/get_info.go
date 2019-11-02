c9483c30f56f3d69fa3ec733f61feda210472c0e
strobus <22136798+strobus@users.noreply.github.com>
2019-11-01 23:30:29 -0400

    refactored server error code

--- handlers/get_info.go
+++ handlers/get_info.go
@@ -1,23 +1,17 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/blocktop/mp-common/server"
 	"github.com/blocktop/mp-transfer-server/config"
 	"io/ioutil"
 	"net/http"
 )
 
-const (
-	// NOFILE is the error code for a file not found
-	NOFILE = "NOFILE"
-)
-
 func HandleGetInfo(w http.ResponseWriter, r *http.Request) {
 	cfg := config.GetConfig()
 	info, err := ioutil.ReadFile(cfg.TransferServerInfoPath)
 	if err != nil {
-		server.ResponseError(w, http.StatusInternalServerError, NOFILE, fmt.Errorf("error reading the MP_TRANSFER_SERVER_INFO_PATH at %s", cfg.TransferServerInfoPath))
+		server.ResponseError(w, http.StatusInternalServerError, server.NOFILE, err)
 		return
 	}
 
